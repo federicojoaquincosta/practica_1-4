cadee205f9f284fc55618c3d0f8f74b2f69fb54e
Federico Costa <federicojoaquincosta@gmail.com>
2022-04-07 10:20:28 -0300

    practica details y links

---
+++ algoritmo.js
@@ -0,0 +1,18 @@
+document.write( `
+    <details>
+        <summary>Info 1</summary>
+        <p>
+            Texto de detalles
+        </p>
+        <img  title="Nagoriyuki_Guilty_Gear_Strive" src="https://i0.wp.com/www.gamerfocus.co/wp-content/uploads/2021/02/Nagoriyuki_Guilty_Gear_Strive.png" alt="Nagoriyuki">
+        <br>
+        <a href="https://twitter.com/" title="twitter">Twitter</a>
+    </details>
+    <details>
+        <summary>Info 2</summary>
+        <p>
+            Texto de detalles
+        </p>
+        
+    </details>
+    `);
